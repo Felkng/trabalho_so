d2be25043486719092cf69f41c0ac4f5d87c5816
felkng <felipersf374@gmail.com>
2025-03-03 15:30:40 -0300

    Use huffman tree to compress e decompress files

--- main.cpp
+++ main.cpp
@@ -1,60 +1,19 @@
 #include <bits/stdc++.h>
+#include "huffman.h"
 using namespace std;
 
-int partition(vector<int> &a, int ini, int fim) {
-    int pivo = a[fim];
-    int i = ini - 1;
-    for (int j = ini; j < fim; j++) {
-        if (a[j] < pivo) {
-            i++;
-            swap(a[j], a[i]);
-        }
-    }
-    i++;
-    swap(a[i], a[fim]);
-    return i;
-}
-
-void quicksort(vector<int> &a, int ini, int fim) {
-    if (ini < fim) {
-        int j = partition(a, ini, fim);
-        quicksort(a, ini, j - 1);
-        quicksort(a, j + 1, fim);
-    }
-}
-
 int main() {
-    vector<int> frequency_table;
-    frequency_table.resize(256, 0);
-    string video = "video.mp4";
-
-    ifstream file(video, ios::binary);
+    HuffmanTree huffman;
     
-    if(!file){
-        cerr << "Não foi possível abrir o arquivo";
-        return 1;
-    }
+    string input = "video.mp4";   // Arquivo original
+    string compressed = "video.huff";  // Arquivo compactado
+    string decompressed = "output.mp4"; // Arquivo descompactado
 
+    huffman.compress(input, compressed);
+    cout << "Arquivo compactado: " << compressed << endl;
 
-    file.seekg(0, ios::end);
-    streamsize file_size = file.tellg();
-    file.seekg(0, ios::beg);
+    huffman.decompress(compressed, decompressed);
+    cout << "Arquivo descompactado: " << decompressed << endl;
 
-    vector<unsigned char> buffer(file_size);
-
-    if(!file.read(reinterpret_cast<char*>(buffer.data()), file_size)){
-        cerr << "Não foi possível ler o arquivo";
-        return 1;
-    }
-
-    for (unsigned char c : buffer) {
-        frequency_table[c]++;
-    }
-
-    quicksort(frequency_table, 0, frequency_table.size() - 1);
-    
-    for (int i = 0; i < frequency_table.size(); i++) cout << "caractere " << "'" << (char)i << "'" << " aparece " << frequency_table[i] << " vezes\n";
-    cout << endl;
-    
     return 0;
 }
